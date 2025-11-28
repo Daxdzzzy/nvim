7e4df3aeffaf47e798e1960c2c20440a26b82268
Daxdzzzy <davidsly829@gmail.com>
2025-11-27 20:59:55 -0500

    Initial project version

---
+++ init.lua
@@ -0,0 +1,6 @@
+require("config.lazy")
+require("config.custom.options")
+require("config.custom.keymaps")
+require("config.custom.diagnostic")
+local opt = vim.opt
+opt.clipboard = "unnamedplus"
---
+++ lua/config/custom/options.lua
@@ -0,0 +1,18 @@
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = true
+opt.inccommand = "split"
+opt.splitbelow = true
+opt.splitright = true
+opt.smartcase = true
+opt.ignorecase = true
+opt.signcolumn = "yes"
+opt.swapfile = false
+opt.wrap = true
+opt.linebreak = true
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.more = false
+opt.mouse = ""
